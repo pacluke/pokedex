3c090af841ac7659ce85ffa98d78339e3291d6ac
pacluke <lucasflores77@gmail.com>
2019-02-06 00:02:37 -0200

    pokemon detail view layout done

---
+++ pokedex/pokedex/Controller/PokemonController.swift
@@ -0,0 +1,9 @@
+//
+//  PokemonController.swift
+//  pokedex
+//
+//  Created by Lucas Flores on 05/02/19.
+//  Copyright © 2019 lucas.flores. All rights reserved.
+//
+
+import Foundation
---
+++ pokedex/pokedex/UI/View/PokemonDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PokemonDetailViewController.swift
+//  pokedex
+//
+//  Created by Lucas Flores on 05/02/19.
+//  Copyright © 2019 lucas.flores. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
